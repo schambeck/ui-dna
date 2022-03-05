4af4ed78925a9c5b737006a3ff16e8f6160c8e27
Marcelo Schambeck <mschambeck@gmail.com>
2022-03-05 03:04:53 -0300

    Extend for small devices

--- src/app/feature/notification-screen/notification-panel/notification-panel.component.ts
+++ src/app/feature/notification-screen/notification-panel/notification-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { OverlayPanel } from 'primeng/overlaypanel';
 import { Observable, Subject, Subscription } from 'rxjs';
@@ -18,6 +18,8 @@ export class NotificationPanelComponent {
   stream$?: Subscription;
   countUnread$ = new Subject<number>();
   selectedNotification?: Notification;
+  @ViewChild('opLg') opLg?: OverlayPanel;
+  @ViewChild('opSm') opSm?: OverlayPanel;
 
   constructor(private notificationService: NotificationService,
     private router: Router) {
@@ -26,14 +28,28 @@ export class NotificationPanelComponent {
     this.notificationService.countUnread().subscribe(this.setCount.bind(this));
   }
 
-  onRowSelect(event: any, panel: OverlayPanel): void {
+  onRowSelect(event: any): void {
     if (event.data) {
       this.notificationService.markAsRead(event.data.id).subscribe({ 
         next: () => console.log(`Marked as read: ${event.data.id}`) 
       });
       this.router.navigate([decodeURI(event.data.link)]);
-      panel.hide();
+      this.getVisiblePanel()?.hide();
     }
+  }
+
+  togglePanel(event: any): void {
+    this.getVisiblePanel()?.toggle(event);
+  }
+
+  getVisiblePanel(): OverlayPanel | undefined {
+    if (this.opLg?.el.nativeElement.offsetParent) {
+      return this.opLg;
+    }
+    if (this.opSm?.el.nativeElement.offsetParent) {
+      return this.opSm;
+    }
+    return undefined;
   }
 
   next(notificationCount: CountUnreadMessage): void {
