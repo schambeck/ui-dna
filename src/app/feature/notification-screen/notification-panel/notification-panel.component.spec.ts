ad4b2be0f74937dda6de5e3d842bd7edac81a512
Marcelo de Moraes Schambeck <mschambeck@gmail.com>
2022-03-08 05:16:36 -0300

    Auth test

--- src/app/feature/notification-screen/notification-panel/notification-panel.component.spec.ts
+++ src/app/feature/notification-screen/notification-panel/notification-panel.component.spec.ts
@@ -1,17 +1,21 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { AuthService } from '@auth0/auth0-angular';
 import { NotificationPanelComponent } from './notification-panel.component';
 
 describe('NotificationPanelComponent', () => {
   let component: NotificationPanelComponent;
   let fixture: ComponentFixture<NotificationPanelComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['loginWithRedirect']);
+
     await TestBed.configureTestingModule({
       declarations: [ NotificationPanelComponent ],
-      imports: [ HttpClientTestingModule, RouterTestingModule ]
+      imports: [ HttpClientTestingModule, RouterTestingModule ],
+      providers: [{ provide: AuthService, useValue: authServiceSpy }]
     })
     .compileComponents();
   });
