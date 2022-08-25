7dc496dd22f7f49f92f5bcf47a929901523c1378
Marcelo Schambeck <mschambeck@gmail.com>
2022-08-24 23:01:01 -0300

    Dev to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   issuer: 'https://sch-srv-authorization-kc.herokuapp.com/auth/realms/schambeck',
   redirectUri: 'https://schambeck.github.io/ui-dna/',
-  urlBase: 'https://sch-dev-api-dna.herokuapp.com',
-  urlBaseNotification: 'https://sch-dev-api-notification.herokuapp.com',
+  urlBase: 'https://sch-api-dna.herokuapp.com',
+  urlBaseNotification: 'https://sch-api-notification.herokuapp.com',
   pollingTimer: 5000
 };
