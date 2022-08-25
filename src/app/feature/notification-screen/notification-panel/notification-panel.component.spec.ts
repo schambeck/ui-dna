a828d3a6ec689cfd84cb886bf28ae99427176071
Marcelo Schambeck <mschambeck@gmail.com>
2022-08-25 00:58:12 -0300

    Get token from localStorage

--- src/app/feature/notification-screen/notification-panel/notification-panel.component.spec.ts
+++ src/app/feature/notification-screen/notification-panel/notification-panel.component.spec.ts
@@ -2,25 +2,24 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from "src/app/auth/auth.service";
-import { OAuthService } from "angular-oauth2-oidc";
+import { OAuthStorage } from "angular-oauth2-oidc";
 import { NotificationPanelComponent } from './notification-panel.component';
-import { Observable, of } from "rxjs";
 
 describe('NotificationPanelComponent', () => {
   let component: NotificationPanelComponent;
   let fixture: ComponentFixture<NotificationPanelComponent>;
-  let oauthServiceSpy: jasmine.SpyObj<OAuthService>;
+  let oAuthStorageSpy: jasmine.SpyObj<OAuthStorage>;
   let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
-    oauthServiceSpy = jasmine.createSpyObj('OAuthService', ['getAccessToken']);
-    authServiceSpy = jasmine.createSpyObj('AuthService', [], {'isAuthenticated$': of(new Observable(subscriber => subscriber.next(true)))});
+    oAuthStorageSpy = jasmine.createSpyObj('OAuthStorage', ['getItem']);
+    authServiceSpy = jasmine.createSpyObj('AuthService', [], ['isAuthenticated$']);
 
     await TestBed.configureTestingModule({
       declarations: [ NotificationPanelComponent ],
       imports: [ HttpClientTestingModule, RouterTestingModule ],
       providers: [
-        { provide: OAuthService, useValue: oauthServiceSpy },
+        { provide: OAuthStorage, useValue: oAuthStorageSpy },
         { provide: AuthService, useValue: authServiceSpy }
       ]
     })
