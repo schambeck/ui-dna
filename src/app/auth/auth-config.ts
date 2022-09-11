71d7762cb732aa32bee411893fef2152b20843ef
Marcelo Schambeck <mschambeck@gmail.com>
2022-09-11 00:34:20 -0300

    No silent refresh

--- src/app/auth/auth-config.ts
+++ src/app/auth/auth-config.ts
@@ -4,15 +4,10 @@ import {environment} from "../../environments/environment";
 export const authConfig: AuthConfig = {
   issuer: environment.issuer,
   clientId: 'newClient', // The "Auth Code + PKCE" client
-  dummyClientSecret: 'newClientSecret', // The "Auth Code + PKCE" client
+  dummyClientSecret: 'newClientSecret',
   responseType: 'code',
-  redirectUri: environment.redirectUri,
-  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
-  scope: 'openid write read', // Ask offline_access to support refresh token refreshes
-  useSilentRefresh: true, // Needed for Code Flow to suggest using iframe-based refreshes
-  silentRefreshTimeout: 5000, // For faster testing
-  timeoutFactor: 0.25, // For faster testing
-  sessionChecksEnabled: true,
+  redirectUri: window.location.origin + '/index.html',
+  scope: 'openid profile email offline_access write read', // Ask offline_access to support refresh token refreshes
   showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
   clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040,
   nonceStateSeparator : 'semicolon', // Real semicolon gets mangled by Duende ID Server's URI encoding
