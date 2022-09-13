335ed5637f27c77e0c9593565cef714b788427ee
Marcelo Schambeck <mschambeck@gmail.com>
2022-09-12 22:06:10 -0300

    Keycloak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  issuer: 'https://sch-srv-authorization.herokuapp.com/auth/realms/schambeck',
+  issuer: 'https://ec2-18-230-88-245.sa-east-1.compute.amazonaws.com/realms/schambeck',
   redirectUri: 'https://schambeck.github.io/ui-dna/',
   urlBase: 'https://sch-api-dna.herokuapp.com',
   urlBaseNotification: 'https://sch-api-notification.herokuapp.com',
