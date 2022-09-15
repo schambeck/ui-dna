2afd4c4c6b10f70e4bb3475d25432bf000a7e32b
Marcelo Schambeck <mschambeck@gmail.com>
2022-09-15 14:30:59 -0300

    Keycloak under a valid SSL certificate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  issuer: 'http://ec2-18-230-88-245.sa-east-1.compute.amazonaws.com/realms/schambeck',
+  issuer: 'https://schambeck.ddns.net/realms/schambeck',
   redirectUri: 'https://schambeck.github.io/ui-dna/',
   urlBase: 'https://sch-api-dna.herokuapp.com',
   urlBaseNotification: 'https://sch-api-notification.herokuapp.com',
