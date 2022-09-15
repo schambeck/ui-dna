63bd166d377850cc6189ffef49c260e6a6fbf535
Marcelo Schambeck <mschambeck@gmail.com>
2022-09-15 14:29:38 -0300

    Scope removed in order to work with social login

--- src/app/auth/auth-config.ts
+++ src/app/auth/auth-config.ts
@@ -7,7 +7,7 @@ export const authConfig: AuthConfig = {
   dummyClientSecret: 'newClientSecret',
   responseType: 'code',
   redirectUri: window.location.origin + '/index.html',
-  scope: 'openid profile email offline_access write read', // Ask offline_access to support refresh token refreshes
+  scope: 'openid profile email read write', // Ask offline_access to support refresh token refreshes
   showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
   clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040,
   nonceStateSeparator : 'semicolon', // Real semicolon gets mangled by Duende ID Server's URI encoding
