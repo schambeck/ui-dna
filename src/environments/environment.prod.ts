04db014af6f4ba1d95742c23b615e4c8039813a9
Marcelo Schambeck <mschambeck@gmail.com>
2022-09-23 09:14:59 -0300

    Revert APIs base URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   issuer: 'https://schambeck.ddns.net/auth/realms/schambeck',
   redirectUri: 'https://schambeck.github.io/ui-dna/',
-  urlBase: 'https://schambeck.ddns.net/api-dna',
-  urlBaseNotification: 'https://schambeck.ddns.net/api-notification',
+  urlBase: 'https://sch-api-dna.herokuapp.com',
+  urlBaseNotification: 'https://sch-api-notification.herokuapp.com',
   pollingTimer: 5000
 };
